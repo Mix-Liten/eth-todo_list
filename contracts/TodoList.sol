0d76b8c0d35b98a93b5176e23fdf197b493ccdcf
Mix-Liten <mixliten@gmail.com>
2022-11-17 11:37:55 +0800

    update contracts/TodoList - sample todo

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.8.17;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() {
+        createTask("Hello World!");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
